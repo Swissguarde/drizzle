79939161453e0739bf71d047284f27244fc38802
Kolade Ayomide <ayomikolade2003@gmail.com>
2023-07-23 10:14:58 +0100

    modifed query function to fetch based on category

--- lib/queries.ts
+++ lib/queries.ts
@@ -1,17 +1,41 @@
 import { firestore } from "@/firebase/clientApp";
-import { Post } from "@/types";
-import { collection, getDocs } from "firebase/firestore";
+import { Project } from "@/types";
+import { collection, getDocs, query, where } from "firebase/firestore";
 
-export async function fetchAllPosts() {
+// export async function fetchAllPosts() {
+//   try {
+//     const postsCollection = collection(firestore, "posts");
+//     const querySnapshot = await getDocs(postsCollection);
+//     const postsResult = querySnapshot.docs.map((doc) => ({
+//       id: doc.id,
+//       ...doc.data(),
+//     }));
+//     return postsResult as Project[];
+//   } catch (error: any) {
+//     console.log("fetchAllPostsError", error.message);
+//   }
+// }
+
+export async function fetchAllPosts(category?: string | null) {
   try {
-    const postsCollection = collection(firestore, "posts");
-    const querySnapshot = await getDocs(postsCollection);
+    let postsRef = collection(firestore, "posts");
+    let postsQuery;
+
+    if (category) {
+      postsQuery = query(postsRef, where("category", "==", category));
+    } else {
+      postsQuery = postsRef;
+    }
+
+    const querySnapshot = await getDocs(postsQuery);
+
     const postsResult = querySnapshot.docs.map((doc) => ({
       id: doc.id,
       ...doc.data(),
     }));
-    return postsResult as Post[];
+
+    return postsResult;
   } catch (error: any) {
-    console.log("fetchAllPostsError", error.message);
+    console.log("fetchCategoryError", error.message);
   }
 }
